a62a04183a5592eccfa8c039e30a115c5f6cf608
Kyle Carter <kylec32@gmail.com>
2015-08-01 09:31:24 -0600

    Trying to get rhcloud compliant

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ var logger = require('morgan');
 var path = require('path');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/recipeholder', function(err) {
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/recipeholder';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect('mongodb://'+connection_string, function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
